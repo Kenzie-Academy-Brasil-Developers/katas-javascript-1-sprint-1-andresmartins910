53646bb5891ca515594fe19e33e5fa1743f74e55
André Martins <andresmartins910@gmail.com>
2021-08-06 13:30:35 -0300

    Katas de JS resolvidos.

--- katas1.js
+++ katas1.js
@@ -1,94 +1,89 @@
-function oneThroughTwenty() {
-    
-   /* Your code goes below
-   Write a for or a while loop
-   return the result*/
-    
+function oneThroughTwenty() { 
+  for (i = 1; i <= 20; i++) {
+    console.log(i)
+  }
 }
 
+oneThroughTwenty()
 //call function oneThroughTwenty
 
 function evensToTwenty() {
-    
-   /* Your code goes below
-   Write a for or a while loop
-   return the result */
-  
+  for (i = 2; i <= 20; i += 2) {
+    console.log(i)
+  }  
 }
 
+evensToTwenty()
 //call function evensToTwenty
 
 function oddsToTwenty() {
-    
-  /* Your code goes below
-  Write a for or a while loop
-  return the result */
-   
+  for (i = 1; i <= 20; i += 2) {
+    console.log(i)
+  }
 }
 
+oddsToTwenty()
 //call function oddsToTwenty
 
-function multiplesOfFive() {
-    
-  /* Your code goes below
-  Write a for or a while loop
-  return the result */
-
+function multiplesOfFive() {    
+  for (i = 5; i <= 100; i += 5) {
+    console.log(i)
+  }
 }
 
-//call function multiplesOfFive
+multiplesOfFive()
+// call function multiplesOfFive
 
 function squareNumbers() {
-    
- /* Your code goes below
-  Write a for or a while loop
-  return the result */
-    
+  for (i = 1 ; i <= 10; i++) {
+    console.log(Math.pow(i, 2))
+  }
 }
 
+squareNumbers()
 //call function squareNumbers
 
 function countingBackwards() {
-    
-  /* Your code goes below
-  Write a for or a while loop
-  return the result */
+  for (i = 20; i > 0; i--){
+    console.log(i)
+  }
 }
 
+countingBackwards()
 //call function countingBackwards
 
 function evenNumbersBackwards() {
-    
- /* Your code goes below
-  Write a for or a while loop
-  return the result */
+    for (i = 20; i > 0; i -= 2) {
+      console.log(i)
+    }
 }
 
+evenNumbersBackwards()
 //call function evenNumbersBackwards
 
 function oddNumbersBackwards() {
-    
- /* Your code goes below
-  Write a for or a while loop
-  return the result */
+  for (i = 19; i > 0; i -= 2) {
+    console.log(i)
+  }
 }
 
+oddNumbersBackwards()
 //call function oddNumbersBackwards
 
 function multiplesOfFiveBackwards() {
-    
- /* Your code goes below
-  Write a for or a while loop
-  return the result */
+  for (i = 100; i > 0; i -= 5) {
+    console.log(i)
+  }
 }
 
+multiplesOfFiveBackwards()
 //call function multiplesOfFiveBackwards
 
 function squareNumbersBackwards() {
-    
-   /* Your code goes below
-  Write a for or a while loop
-  return the result */
+  for (i = 10 ; i > 0; i--) {
+    console.log(Math.pow(i, 2))
+  }
 }
 
+squareNumbersBackwards()
 //call function squareNumbersBackwards
